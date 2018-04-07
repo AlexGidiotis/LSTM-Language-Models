3f87cea7e03800498bb851ff5f37c0657365ac82
Alex Gidiotis <gidiotisAlex@outlook.com.gr>
2018-04-07 21:41:44 +0300

    added some callbacks

--- language_model.py
+++ language_model.py
@@ -7,13 +7,67 @@ from keras.layers import Dense, Input, LSTM, Dropout, Activation
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 import keras.backend as K
-
+from keras import callbacks
 from data_gen import Corpus
 
 
 
 train_data = 'data/train_set.txt'
 val_data = 'data/val_set.txt'
+
+
+class GenerateText(callbacks.Callback):
+	"""
+	"""
+
+	def on_epoch_end(self, epoch, logs={}):
+		"""
+		"""		
+
+		starting_text = 'WikiCorpus language model'
+
+		if epoch % 50 == 0:
+			test_generated = ''
+			test_generated += starting_text
+			sys.stdout.write(test_generated)
+
+			# Generate 1000 characters.
+			for i in range(1000):
+				# First vectorize the starting text.
+				x = np.zeros((1, max_len, data_set.vocab_size))
+
+				for t, char in enumerate(starting_text):
+					x[0, t, data_set.char2id[char]] = 1.
+
+				# Predict using the previously generated text as input.
+				preds = model.predict(x, verbose=0)[0]
+				# Sample and decode the prediction.
+				next_char_one_hot = sample(preds,0.6)
+				next_char = data_set.id2char[np.argmax(next_char_one_hot)]
+				# Append prediction to the generated text.
+				test_generated += next_char
+				# Write the output one char at a time.
+				sys.stdout.write(next_char)
+				sys.stdout.flush()
+
+			print()
+
+		return
+
+
+class Checkpointer(callbacks.Callback):
+	"""
+	"""
+
+	def on_epoch_end(self, epoch, logs={}):
+		"""
+		"""		
+
+		if epoch % 10 == 0:
+			model.save_weights(STAMP + '.hdf5', True)
+
+		return
+ 
 
 def sample(prediction,
 	temperature):
@@ -67,7 +121,6 @@ def build_model(max_len,
 
 if __name__ == '__main__':
 
-	starting_text = 'WikiCorpus language model'
 	epochs = 2000
 	max_len = 25
 	batch_size = 512
@@ -84,48 +137,17 @@ if __name__ == '__main__':
 	with open(STAMP + ".json", "w") as json_file:
 		json_file.write(model_json)
 
-	# The training process
-	for epoch in range(epochs):
-		print 'Epoch:',epoch
-		# Train for one iteration over all batches
-
-		hist = model.fit_generator(data_set.get_train(),
-			steps_per_epoch=(data_set.train_size),
-			epochs=1,
-			validation_data=data_set.get_val(),
-			validation_steps=(data_set.val_size),
-			callbacks=[],
-			verbose=1)
+	generate_text = GenerateText()
+	checkpointer = Checkpointer()
+	
+	hist = model.fit_generator(data_set.get_train(),
+		steps_per_epoch=(data_set.train_size),
+		epochs=epochs,
+		validation_data=data_set.get_val(),
+		validation_steps=(data_set.val_size),
+		callbacks=[generate_text,checkpointer],
+		verbose=1)
 
 
-		# Save every 10 epochs
-		if epoch % 10 == 0:
-			model.save_weights(STAMP+'.hdf5', True)
-
-		# Generate text
-		if epoch % 50 == 0:
-			test_generated = ''
-			test_generated += starting_text
-			sys.stdout.write(test_generated)
-
-			# Generate 1000 characters.
-			for i in range(1000):
-				# First vectorize the starting text.
-				x = np.zeros((1, max_len, data_set.vocab_size))
-
-				for t, char in enumerate(starting_text):
-					x[0, t, data_set.char2id[char]] = 1.
-
-				# Predict using the previously generated text as input.
-				preds = model.predict(x, verbose=0)[0]
-				# Sample and decode the prediction.
-				next_char_one_hot = sample(preds,0.6)
-				next_char = data_set.id2char[np.argmax(next_char_one_hot)]
-				# Append prediction to the generated text.
-				test_generated += next_char
-				# Write the output one char at a time.
-				sys.stdout.write(next_char)
-				sys.stdout.flush()
-
-			print()
+		
 
